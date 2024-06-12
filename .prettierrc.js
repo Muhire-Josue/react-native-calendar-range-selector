10f1547d4d6eb432c50833355fb12247215db251
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-06-12 11:28:50 +0200

    - Adds some basic configuration

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  // trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  parser: 'babel',
+  filepath: '',
+  requireConfig: false,
+  insertPragma: false,
+  requirePragma: false,
+  plugins: [],
+  htmlWhitespaceSensitivity: 'css',
+
+  vueIndentScriptAndStyle: false,
+
+  endOfLine: 'lf',
+};
